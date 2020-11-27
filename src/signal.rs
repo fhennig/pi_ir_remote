425af6c3eb489430967a333b4c9b6369a7c3af88
Felix hennig <mail@felixhennig.com>
2020-11-27 16:53:53 +0100

    Refactored out string conversion

--- src/signal.rs
+++ src/signal.rs
@@ -1,11 +1,10 @@
 /// A signal is a button press that has been transmitted from the remote,
-/// but might also include things like "held down" to indicate that the 
+/// but might also include things like "held down" to indicate that the
 /// last pressed button is being held down.
-/// 
+///
 /// On a technical level, a signal is encoded in pulses of different
 /// lengths.
 use crate::Pulse;
-use crate::pulse::seq_to_string;
 
 /// The different buttons on the remote, plus an "Unrecognized" signal.
 #[derive(Debug, Clone, Copy, PartialEq)]
@@ -60,53 +59,55 @@ pub enum Signal {
 impl Signal {
     /// Attempts to match a pulse sequence to a signal.  All button codes
     /// are 32 pulses long (32 bits).
-    pub fn from_pulse_seq(pulse_seq: &Vec<Pulse>) -> Signal {
-        let seq_as_str = seq_to_string(pulse_seq);
-        match seq_as_str.as_str() {
-            "SSSSSSSSLLLLLLLLSSSSSSLSLLLLLLSL" => Signal::Power,
-            "SSSSSSSSLLLLLLLLLSSSSSLSSLLLLLSL" => Signal::PlayPause,
-            "SSSSSSSSLLLLLLLLLSLLLSLSSLSSSLSL" => Signal::BrightnessDown,
-            "SSSSSSSSLLLLLLLLSSLLLSLSLLSSSLSL" => Signal::BrightnessUp,
-            "SSSSSSSSLLLLLLLLSSLSSSLSLLSLLLSL" => Signal::White,
-            "SSSSSSSSLLLLLLLLSSSLLSLSLLLSSLSL" => Signal::Red,
-            "SSSSSSSSLLLLLLLLLSSLLSLSSLLSSLSL" => Signal::Green,
-            "SSSSSSSSLLLLLLLLLSLSSSLSSLSLLLSL" => Signal::Blue,
-            "SSSSSSSSLLLLLLLLSSLSLSLSLLSLSLSL" => Signal::Orange1,
-            "SSSSSSSSLLLLLLLLSSSSLSLSLLLLSLSL" => Signal::Orange2,
-            "SSSSSSSSLLLLLLLLSSLLLSSSLLSSSLLL" => Signal::Orange3,
-            "SSSSSSSSLLLLLLLLSSSLLSSSLLLSSLLL" => Signal::Yellow,
-            "SSSSSSSSLLLLLLLLLSLSLSLSSLSLSLSL" => Signal::GrassGreen,
-            "SSSSSSSSLLLLLLLLLSSSLSLSSLLLSLSL" => Signal::Turquise,
-            "SSSSSSSSLLLLLLLLLSLLLSSSSLSSSLLL" => Signal::Petrol,
-            "SSSSSSSSLLLLLLLLLSSLLSSSSLLSSLLL" => Signal::DarkPetrol,
-            "SSSSSSSSLLLLLLLLLSSLSSLSSLLSLLSL" => Signal::Blue2,
-            "SSSSSSSSLLLLLLLLLSLLSSLSSLSSLLSL" => Signal::Violet,
-            "SSSSSSSSLLLLLLLLSLLLLSSSLSSSSLLL" => Signal::LightViolet,
-            "SSSSSSSSLLLLLLLLSLSLLSSSLSLSSLLL" => Signal::Pink,
-            "SSSSSSSSLLLLLLLLSSSLSSLSLLLSLLSL" => Signal::Rose1,
-            "SSSSSSSSLLLLLLLLSSLLSSLSLLSSLLSL" => Signal::Rose2,
-            "SSSSSSSSLLLLLLLLLLLLLSSSSSSSSLLL" => Signal::Azure1,
-            "SSSSSSSSLLLLLLLLLLSLLSSSSSLSSLLL" => Signal::Azure2,
-            "SSSSSSSSLLLLLLLLSSLSLSSSLLSLSLLL" => Signal::RedUp,
-            "SSSSSSSSLLLLLLLLSSSSLSSSLLLLSLLL" => Signal::RedDown,
-            "SSSSSSSSLLLLLLLLLSLSLSSSSLSLSLLL" => Signal::GreenUp,
-            "SSSSSSSSLLLLLLLLLSSSLSSSSLLLSLLL" => Signal::GreenDown,
-            "SSSSSSSSLLLLLLLLSLLSLSSSLSSLSLLL" => Signal::BlueUp,
-            "SSSSSSSSLLLLLLLLSLSSLSSSLSLLSLLL" => Signal::BlueDown,
-            "SSSSSSSSLLLLLLLLLLLSLSSSSSSLSLLL" => Signal::Quick,
-            "SSSSSSSSLLLLLLLLLLSSLSSSSSLLSLLL" => Signal::Slow,
-            "SSSSSSSSLLLLLLLLSSLLSSSSLLSSLLLL" => Signal::Diy1,
-            "SSSSSSSSLLLLLLLLLSLLSSSSSLSSLLLL" => Signal::Diy2,
-            "SSSSSSSSLLLLLLLLSLLLSSSSLSSSLLLL" => Signal::Diy3,
-            "SSSSSSSSLLLLLLLLSSSLSSSSLLLSLLLL" => Signal::Diy4,
-            "SSSSSSSSLLLLLLLLLSSLSSSSSLLSLLLL" => Signal::Diy5,
-            "SSSSSSSSLLLLLLLLSLSLSSSSLSLSLLLL" => Signal::Diy6,
-            "SSSSSSSSLLLLLLLLLLLLSSSSSSSSLLLL" => Signal::Auto,
-            "SSSSSSSSLLLLLLLLLLSLSSSSSSLSLLLL" => Signal::Flash,
-            "SSSSSSSSLLLLLLLLSSLSSSSSLLSLLLLL" => Signal::Jump3,
-            "SSSSSSSSLLLLLLLLLSLSSSSSSLSLLLLL" => Signal::Jump7,
-            "SSSSSSSSLLLLLLLLSLLSSSSSLSSLLLLL" => Signal::Fade3,
-            "SSSSSSSSLLLLLLLLLLLSSSSSSSSLLLLL" => Signal::Fade7,
+    #[rustfmt::skip]
+    pub fn from_pulse_seq(pulse_seq: &[Pulse]) -> Signal {
+        use Pulse::Long as L;
+        use Pulse::Short as S;
+        match pulse_seq {
+            [S,S,S,S,S,S,S,S,L,L,L,L,L,L,L,L,S,S,S,S,S,S,L,S,L,L,L,L,L,L,S,L] => Signal::Power,
+            [S,S,S,S,S,S,S,S,L,L,L,L,L,L,L,L,L,S,S,S,S,S,L,S,S,L,L,L,L,L,S,L] => Signal::PlayPause,
+            [S,S,S,S,S,S,S,S,L,L,L,L,L,L,L,L,L,S,L,L,L,S,L,S,S,L,S,S,S,L,S,L] => Signal::BrightnessDown,
+            [S,S,S,S,S,S,S,S,L,L,L,L,L,L,L,L,S,S,L,L,L,S,L,S,L,L,S,S,S,L,S,L] => Signal::BrightnessUp,
+            [S,S,S,S,S,S,S,S,L,L,L,L,L,L,L,L,S,S,L,S,S,S,L,S,L,L,S,L,L,L,S,L] => Signal::White,
+            [S,S,S,S,S,S,S,S,L,L,L,L,L,L,L,L,S,S,S,L,L,S,L,S,L,L,L,S,S,L,S,L] => Signal::Red,
+            [S,S,S,S,S,S,S,S,L,L,L,L,L,L,L,L,L,S,S,L,L,S,L,S,S,L,L,S,S,L,S,L] => Signal::Green,
+            [S,S,S,S,S,S,S,S,L,L,L,L,L,L,L,L,L,S,L,S,S,S,L,S,S,L,S,L,L,L,S,L] => Signal::Blue,
+            [S,S,S,S,S,S,S,S,L,L,L,L,L,L,L,L,S,S,L,S,L,S,L,S,L,L,S,L,S,L,S,L] => Signal::Orange1,
+            [S,S,S,S,S,S,S,S,L,L,L,L,L,L,L,L,S,S,S,S,L,S,L,S,L,L,L,L,S,L,S,L] => Signal::Orange2,
+            [S,S,S,S,S,S,S,S,L,L,L,L,L,L,L,L,S,S,L,L,L,S,S,S,L,L,S,S,S,L,L,L] => Signal::Orange3,
+            [S,S,S,S,S,S,S,S,L,L,L,L,L,L,L,L,S,S,S,L,L,S,S,S,L,L,L,S,S,L,L,L] => Signal::Yellow,
+            [S,S,S,S,S,S,S,S,L,L,L,L,L,L,L,L,L,S,L,S,L,S,L,S,S,L,S,L,S,L,S,L] => Signal::GrassGreen,
+            [S,S,S,S,S,S,S,S,L,L,L,L,L,L,L,L,L,S,S,S,L,S,L,S,S,L,L,L,S,L,S,L] => Signal::Turquise,
+            [S,S,S,S,S,S,S,S,L,L,L,L,L,L,L,L,L,S,L,L,L,S,S,S,S,L,S,S,S,L,L,L] => Signal::Petrol,
+            [S,S,S,S,S,S,S,S,L,L,L,L,L,L,L,L,L,S,S,L,L,S,S,S,S,L,L,S,S,L,L,L] => Signal::DarkPetrol,
+            [S,S,S,S,S,S,S,S,L,L,L,L,L,L,L,L,L,S,S,L,S,S,L,S,S,L,L,S,L,L,S,L] => Signal::Blue2,
+            [S,S,S,S,S,S,S,S,L,L,L,L,L,L,L,L,L,S,L,L,S,S,L,S,S,L,S,S,L,L,S,L] => Signal::Violet,
+            [S,S,S,S,S,S,S,S,L,L,L,L,L,L,L,L,S,L,L,L,L,S,S,S,L,S,S,S,S,L,L,L] => Signal::LightViolet,
+            [S,S,S,S,S,S,S,S,L,L,L,L,L,L,L,L,S,L,S,L,L,S,S,S,L,S,L,S,S,L,L,L] => Signal::Pink,
+            [S,S,S,S,S,S,S,S,L,L,L,L,L,L,L,L,S,S,S,L,S,S,L,S,L,L,L,S,L,L,S,L] => Signal::Rose1,
+            [S,S,S,S,S,S,S,S,L,L,L,L,L,L,L,L,S,S,L,L,S,S,L,S,L,L,S,S,L,L,S,L] => Signal::Rose2,
+            [S,S,S,S,S,S,S,S,L,L,L,L,L,L,L,L,L,L,L,L,L,S,S,S,S,S,S,S,S,L,L,L] => Signal::Azure1,
+            [S,S,S,S,S,S,S,S,L,L,L,L,L,L,L,L,L,L,S,L,L,S,S,S,S,S,L,S,S,L,L,L] => Signal::Azure2,
+            [S,S,S,S,S,S,S,S,L,L,L,L,L,L,L,L,S,S,L,S,L,S,S,S,L,L,S,L,S,L,L,L] => Signal::RedUp,
+            [S,S,S,S,S,S,S,S,L,L,L,L,L,L,L,L,S,S,S,S,L,S,S,S,L,L,L,L,S,L,L,L] => Signal::RedDown,
+            [S,S,S,S,S,S,S,S,L,L,L,L,L,L,L,L,L,S,L,S,L,S,S,S,S,L,S,L,S,L,L,L] => Signal::GreenUp,
+            [S,S,S,S,S,S,S,S,L,L,L,L,L,L,L,L,L,S,S,S,L,S,S,S,S,L,L,L,S,L,L,L] => Signal::GreenDown,
+            [S,S,S,S,S,S,S,S,L,L,L,L,L,L,L,L,S,L,L,S,L,S,S,S,L,S,S,L,S,L,L,L] => Signal::BlueUp,
+            [S,S,S,S,S,S,S,S,L,L,L,L,L,L,L,L,S,L,S,S,L,S,S,S,L,S,L,L,S,L,L,L] => Signal::BlueDown,
+            [S,S,S,S,S,S,S,S,L,L,L,L,L,L,L,L,L,L,L,S,L,S,S,S,S,S,S,L,S,L,L,L] => Signal::Quick,
+            [S,S,S,S,S,S,S,S,L,L,L,L,L,L,L,L,L,L,S,S,L,S,S,S,S,S,L,L,S,L,L,L] => Signal::Slow,
+            [S,S,S,S,S,S,S,S,L,L,L,L,L,L,L,L,S,S,L,L,S,S,S,S,L,L,S,S,L,L,L,L] => Signal::Diy1,
+            [S,S,S,S,S,S,S,S,L,L,L,L,L,L,L,L,L,S,L,L,S,S,S,S,S,L,S,S,L,L,L,L] => Signal::Diy2,
+            [S,S,S,S,S,S,S,S,L,L,L,L,L,L,L,L,S,L,L,L,S,S,S,S,L,S,S,S,L,L,L,L] => Signal::Diy3,
+            [S,S,S,S,S,S,S,S,L,L,L,L,L,L,L,L,S,S,S,L,S,S,S,S,L,L,L,S,L,L,L,L] => Signal::Diy4,
+            [S,S,S,S,S,S,S,S,L,L,L,L,L,L,L,L,L,S,S,L,S,S,S,S,S,L,L,S,L,L,L,L] => Signal::Diy5,
+            [S,S,S,S,S,S,S,S,L,L,L,L,L,L,L,L,S,L,S,L,S,S,S,S,L,S,L,S,L,L,L,L] => Signal::Diy6,
+            [S,S,S,S,S,S,S,S,L,L,L,L,L,L,L,L,L,L,L,L,S,S,S,S,S,S,S,S,L,L,L,L] => Signal::Auto,
+            [S,S,S,S,S,S,S,S,L,L,L,L,L,L,L,L,L,L,S,L,S,S,S,S,S,S,L,S,L,L,L,L] => Signal::Flash,
+            [S,S,S,S,S,S,S,S,L,L,L,L,L,L,L,L,S,S,L,S,S,S,S,S,L,L,S,L,L,L,L,L] => Signal::Jump3,
+            [S,S,S,S,S,S,S,S,L,L,L,L,L,L,L,L,L,S,L,S,S,S,S,S,S,L,S,L,L,L,L,L] => Signal::Jump7,
+            [S,S,S,S,S,S,S,S,L,L,L,L,L,L,L,L,S,L,L,S,S,S,S,S,L,S,S,L,L,L,L,L] => Signal::Fade3,
+            [S,S,S,S,S,S,S,S,L,L,L,L,L,L,L,L,L,L,L,S,S,S,S,S,S,S,S,L,L,L,L,L] => Signal::Fade7,
             _ => Signal::Unrecognized,
         }
     }
